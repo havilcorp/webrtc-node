ad0ef0e093f0b0d95ec21ef0319e346bdd6c23b8
havilcorp <havilcorp@gmail.com>
2023-11-02 00:20:45 +0300

    asd

--- public/script.js
+++ public/script.js
@@ -22,8 +22,8 @@ showChat.addEventListener('click', () => {
 const user = prompt('Enter your name')
 
 var peer = new Peer({
-	host: '127.0.0.1',
-	port: 3030,
+	host: 'demonstration.monster',
+	port: 443,
 	path: '/peerjs',
 	config: {
 		iceServers: [
